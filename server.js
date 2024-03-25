e55ab2866dc76a3b29ebf85f7efb947d44937f6c
Said <alhendisaid@gmail.com>
2024-03-25 15:50:15 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import jsonServer from 'json-server'
+import express from 'express'
+
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use((req, res, next) => {
+  setTimeout(next, 500)
+})
+server.use(router)
+
+const app = express()
+app.use(server)
+
+const PORT = 8080
+app.listen(PORT, () => {
+  console.log('Server is running on...')
+})
